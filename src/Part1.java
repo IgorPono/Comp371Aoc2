e251d3f1f828406d45f8ff4f40484f43904d22e7
Igor Ponomarenko <70162883+IgorPono@users.noreply.github.com>
2023-12-07 00:01:56 -0600

    Part 1 and Part 2 Solutions

---
+++ src/Part1.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Part1 {
+
+    public static Pattern pattern = Pattern.compile("(\\d+) (\\w+)");
+
+    public static void main(String[] args) throws Exception {
+        /*String input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green \n" +
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" +
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" +
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" +
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\n";*/
+
+        String filePath = "day2InputPart1.txt";
+        String input = readFileToString(filePath);
+
+        int sum = Arrays.stream(input.split("\n"))
+                .map(line -> line.split(":"))
+                .mapToInt(arr -> {
+                    int gameNum = Integer.parseInt(arr[0].replace("Game", "").trim());
+                    int[] counts = Arrays.stream(arr[1].split(";"))
+                            .flatMap(rounds -> Arrays.stream(rounds.split(",")))
+                            .map(pattern::matcher)
+                            .filter(Matcher::find)
+                            .collect(() -> new int[3], (acc, m) -> {
+                                int quantity = Integer.parseInt(m.group(1));
+                                String color = m.group(2);
+                                switch (color) {
+                                    case "blue":
+                                        acc[0] = Math.max(acc[0], quantity);
+                                        break;
+                                    case "red":
+                                        acc[1] = Math.max(acc[1], quantity);
+                                        break;
+                                    case "green":
+                                        acc[2] = Math.max(acc[2], quantity);
+                                        break;
+                                }
+                            }, (left, right) -> {
+                                left[0] = Math.max(left[0], right[0]);
+                                left[1] = Math.max(left[1], right[1]);
+                                left[2] = Math.max(left[2], right[2]);
+                            });
+
+                    return (counts[0] <= 14 && counts[1] <= 12 && counts[2] <= 13) ? gameNum : 0;
+                })
+                .sum();
+
+        System.out.println(sum);
+    }
+
+    private static String readFileToString(String filePath) throws Exception {
+        Path path = Paths.get(filePath);
+        byte[] fileBytes = Files.readAllBytes(path);
+        return new String(fileBytes);
+    }
+}
